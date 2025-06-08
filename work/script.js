0d370caabb72b14506a526647cee066deed99c0f
ArtemGames009 <pechonchuka@gmail.com>
2025-06-08 20:46:54 +0300

    Add files via upload

---
+++ work/script.js
@@ -0,0 +1,9 @@
+
+document.getElementById('Button1').addEventListener('click', function () {
+    window.location.href = 'blockdiagram2.html';
+});
+
+
+document.querySelector('.back[id="@Button1"]').addEventListener('click', function () {
+    window.location.href = 'blockdiagram.html';
+});
